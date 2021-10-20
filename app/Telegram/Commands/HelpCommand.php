1324b3bdf5b08c219a85249c2ecf8e35b6513e1c
abenezer-BGI <abenezer.ayalneh@castel-afrique.com>
2021-10-20 11:56:03 +0300

    fix

--- app/Telegram/Commands/HelpCommand.php
+++ app/Telegram/Commands/HelpCommand.php
@@ -33,12 +33,13 @@ class HelpCommand extends Command
     {
         $response = $this->getUpdate();
 
+
         $text = 'Hey stranger, thanks for visiting me.'.chr(10).chr(10);
         $text .= 'I am a bot and working for'.chr(10);
         $text .= env('APP_URL').chr(10).chr(10);
         $text .= 'Please come and visit me there.'.chr(10);
 
-        $this->replyWithMessage(compact('text'));
+        $this->replyWithMessage($response);
 
     }
 }
